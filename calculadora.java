473720f3c52e6d27d2887e172d1ec41911864b8b
alumno <hernandezsierrarafa@gmail.com>
2023-09-19 19:47:40 +0200

    se añade a calculadora la raiz cuadrada

--- calculadora.java
+++ calculadora.java
@@ -2,6 +2,8 @@ package calculadora;
 
 import java.util.*;
 
+import java.math.*;
+
 public class calculadora {	
 	
 	public static void main(String[] args) {
@@ -12,13 +14,21 @@ public class calculadora {
 		
 		char simbolo;
 		
-		System.out.print("Escribe el primer numero ");
-		
-		num1= s.nextDouble();
-				
-		System.out.print("Escribe el segundo numero ");
-		
-		num2= s.nextDouble();		
+        System.out.print("Escribe el primer numero(si quieres hacer la raiz no importa el numero que pongas segundo): ");
+        try {
+            num1 = s.nextDouble();
+        } catch (InputMismatchException e) {
+            System.out.println("Error: Ingresa un número válido.");
+            return;
+        }
+        
+        System.out.print("Escribe el segundo numero: ");
+        try {
+            num2 = s.nextDouble();
+        } catch (InputMismatchException e) {
+            System.out.println("Error: Ingresa un número válido.");
+            return;
+        }		
 		
 		System.out.println("Escribe el simbolo de la operacion ");
 		
@@ -38,12 +48,20 @@ public class calculadora {
 		case '/':
 			resultado=num1/num2;
 			break;
+		case '√':
+			if (num1 < 0) {
+                throw new Error("No se puede calcular la raíz cuadrada de un número negativo");
+            }
+			resultado=Math.sqrt(num1);
+			
+			break;
+			
+		default:
+            System.out.println("Error: Operador no válido.");
+            return;
 		}		
 		
 		System.out.println("El resultado es "+resultado);
 	}
 	
 	}
-	
-
-
